c9787a83eeb996d76e2978c5a86e4b4dd59dfb1b
gunwunbun <jvanhouts@protonmail.com>
2023-05-09 15:27:21 +0200

    added contact info and google maps map

--- resources/js/Pages/Contact.svelte
+++ resources/js/Pages/Contact.svelte
@@ -6,4 +6,8 @@
     <title>The Golden Dragon</title>
 </svelte:head>
 
-Inhoud contactpagina
+<p class="text-xl font-bold">De Gouden Draak is eenvoudig te vinden, vlak bij het centrum, 5 minuten lopen achter het centraal station.</p>
+<p class="text-lg mt-4 font-bold">Onderwijsboulevard 215, kamer OG112</p>
+<p class="text-lg mb-4 font-bold -mt-0.5">5223 DE 's-Hertogenbosch </p>
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2473.432987711052!2d5.284448915590253!3d51.68852110526529!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c6ee8855f3c5d1%3A0x4c4797f27d227e73!2sOnderwijsboulevard%20215%2C%205223%20DE%20&#39;s-Hertogenbosch!5e0!3m2!1sen!2snl!4v1585055473064!5m2!1sen!2snl" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
