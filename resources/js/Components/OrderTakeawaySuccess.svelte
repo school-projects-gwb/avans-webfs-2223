c23846eba88490e15e27e6370da55827aacdbca6
gunwunbun <jvanhouts@protonmail.com>
2023-05-17 12:40:53 +0200

    added qr code

--- resources/js/Components/OrderTakeawaySuccess.svelte
+++ resources/js/Components/OrderTakeawaySuccess.svelte
@@ -1,27 +1,43 @@
 <script>
     import axios from "axios";
     import {onMount} from "svelte";
+    import QrCode from "svelte-qrcode"
 
     let orderPlaced = false;
     let orderData;
 
     onMount(async () => {
+        await handleQrCode();
+    });
+
+    async function handleQrCode() {
         axios.get('/cart/is-order-placed').then(response => {
             orderPlaced = response.data['order'] != null;
 
             if (!orderPlaced) return;
+            orderData = response.data['order'];
 
-            axios.get('/cart/get-order-qr-data').then(response => {
-                // todo implement
-            });
+            let orderLineData = '';
+            for (let orderLine of orderData['order_lines']) {
+                orderLineData += `dish-${orderLine['dish_id']}_option-${orderLine['option_id']}_x${orderLine['amount']}`;
+            }
+
+            orderData = `${orderData['first_name']}_${orderData['last_name']}_#${orderData['id']}_`;
+            orderData += orderLineData;
         });
-    });
+    }
 
     export const handleOrderPlaced = async (orderData) => {
-        orderPlaced = true;
+        await handleQrCode();
     }
 </script>
 
 {#if orderPlaced}
-    <button class="bg-primary text-white py-2 px-4 text-xl uppercase border-none font-bold">Nieuwe bestelling starten</button>
+    <div class="flex flex-col">
+        <h1 class="text-4xl font-bold text-primary text-left">Bestelling succesvol geplaatst!</h1>
+        <div class="w-1/2 my-4">
+            <QrCode value="{orderData}" />
+        </div>
+        <button class="bg-primary text-white py-2 px-4 text-xl uppercase border-none font-bold mt-4">Nieuwe bestelling starten</button>
+    </div>
 {/if}
