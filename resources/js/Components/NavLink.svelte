13077aa3911844817426837a42a4783c6753ffae
gunwunbun <jvanhouts@protonmail.com>
2023-06-15 08:58:44 +0200

    nav fix

--- resources/js/Components/NavLink.svelte
+++ resources/js/Components/NavLink.svelte
@@ -4,9 +4,7 @@
     export let href = null,
         active = false;
 
-    const classes = active
-        ? "inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out"
-        : "inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out";
+    const classes = "inline-flex h-fit items-center px-4 xl:px-8 py-0 rounded-lg border border-blue-600 text-blue-600 font-bold bg-blue-100";
 </script>
 
 <Link {href} class={classes}>
--- resources/js/Layouts/AuthenticatedLayout.svelte
+++ resources/js/Layouts/AuthenticatedLayout.svelte
@@ -1,10 +1,9 @@
 <script>
     import { Link, page } from "@inertiajs/svelte";
-
+    import GoodpayLogo from '../../img/goodpay.png'
     import NavLink from "../Components/NavLink.svelte";
     import Dropdown from "../Components/Dropdown.svelte";
     import DropdownLink from "../Components/DropdownLink.svelte";
-    import ApplicationLogo from "../Components/ApplicationLogo.svelte";
     import ResponsiveNavLink from "../Components/ResponsiveNavLink.svelte";
     import {onMount} from "svelte";
     import axios from "axios";
@@ -31,24 +30,13 @@
         <nav
             class="bg-white border-b border-gray-100"
         >
-            <!-- Primary Navigation Menu -->
-            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div class="flex justify-between h-16">
+            <div class="mx-auto px-4 sm:px-6 lg:px-8">
+                <div class="flex flex-col lg:flex-row justify-between items-center py-2">
+                    <img class="flex h-12" src="{GoodpayLogo}">
                     <div class="flex">
-                        <!-- Logo -->
-                        <div class="shrink-0 flex items-center">
-                            <Link href={route("dashboard")}>
-                                <ApplicationLogo
-                                    classes="block h-9 w-auto fill-current text-gray-800"
-                                />
-                            </Link>
-                        </div>
-
-                        <!-- Navigation Links -->
                         <div
-                            class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex"
+                            class="lg:space-x-4 xl:space-x-8 mt-4 lg:mt-0 space-y-2 lg:space-y-0 sm:-my-px lg:ml-10 flex flex-col lg:flex-row"
                         >
-                            <!-- TODO: work on favicon -->
                             <NavLink
                                 href={route("dashboard")}
                                 active={route().current("dashboard")}
@@ -93,7 +81,7 @@
                                 >
                                     Verkoopoverzicht
                                 </NavLink>
-                                
+
                                 <NavLink
                                     href={route("news.index")}
                                     active={route().current("news.index")}
@@ -122,18 +110,15 @@
                                     Reviews
                                 </NavLink>
                             {/if}
-                        </div>
-                    </div>
 
-                    <div class="hidden sm:flex sm:items-center sm:ml-6">
-                        <!-- Settings Dropdown -->
-                        <div class="ml-3 relative">
-                            <Dropdown align="right" width="48">
-                                <svelte:fragment slot="trigger">
+                            <div class="flex sm:items-center lg:ml-6">
+                                <div class="ml-3 relative inline-flex h-fit items-center px-8 py-0 rounded-lg border border-blue-600 text-blue-600 font-bold bg-blue-100">
+                                    <Dropdown align="right" width="48">
+                                        <svelte:fragment slot="trigger">
                                     <span class="inline-flex rounded-md">
                                         <button
                                             type="button"
-                                            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                                            class="inline-flex items-center bg-blue-100 text-blue-600 font-bold border border-transparent text-sm leading-4 font-medium rounded-md hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
                                         >
                                             {$page.props.auth.user.name}
 
@@ -151,106 +136,27 @@
                                             </svg>
                                         </button>
                                     </span>
-                                </svelte:fragment>
+                                        </svelte:fragment>
 
-                                <svelte:fragment slot="content">
-                                    <!-- content here -->
-                                    <DropdownLink href={route("profile.edit")}>
-                                        Profile
-                                    </DropdownLink>
-                                    <DropdownLink
-                                        href={route("logout")}
-                                        method="post"
-                                        as="button"
-                                    >
-                                        Log Out
-                                    </DropdownLink>
-                                </svelte:fragment>
-                            </Dropdown>
+                                        <svelte:fragment slot="content">
+                                            <!-- content here -->
+                                            <DropdownLink href={route("profile.edit")}>
+                                                Profile
+                                            </DropdownLink>
+                                            <DropdownLink
+                                                href={route("logout")}
+                                                method="post"
+                                                as="button"
+                                            >
+                                                Log Out
+                                            </DropdownLink>
+                                        </svelte:fragment>
+                                    </Dropdown>
+                                </div>
+                            </div>
                         </div>
                     </div>
-
-                    <!-- Hamburger -->
-                    <div class="-mr-2 flex items-center sm:hidden">
-                        <button
-                            on:click={() => {
-                                showingNavigationDropdown =
-                                    !showingNavigationDropdown;
-                            }}
-                            class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
-                        >
-                            <svg
-                                class="h-6 w-6"
-                                stroke="currentColor"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                            >
-                                <path
-                                    class={showingNavigationDropdown
-                                        ? "hidden"
-                                        : "inline-flex"}
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M4 6h16M4 12h16M4 18h16"
-                                />
-                                <path
-                                    class={showingNavigationDropdown
-                                        ? "hidden"
-                                        : "inline-flex"}
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M6 18L18 6M6 6l12 12"
-                                />
-                            </svg>
-                        </button>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Responsive Navigation Menu -->
-            <div
-                class="{showingNavigationDropdown
-                    ? 'block'
-                    : 'hidden'} sm:hidden"
-            >
-                <div class="pt-2 pb-3 space-y-1">
-                    <ResponsiveNavLink
-                        href={route("dashboard")}
-                        active={route().current("dashboard")}
-                    >
-                        Dashboard
-                    </ResponsiveNavLink>
-                </div>
-
-                <!-- Responsive Settings Options -->
-                <div
-                    class="pt-4 pb-1 border-t border-gray-200"
-                >
-                    <div class="px-4">
-                        <div
-                            class="font-medium text-base text-gray-800"
-                        >
-                            {$page.props.auth.user.name}
-                        </div>
-                        <div class="font-medium text-sm text-gray-500">
-                            {$page.props.auth.user.email}
-                        </div>
-                    </div>
-
-                    <div class="mt-3 space-y-1">
-                        <ResponsiveNavLink href={route("profile.edit")}>
-                            Profile
-                        </ResponsiveNavLink>
-                        <ResponsiveNavLink
-                            href={route("logout")}
-                            method="post"
-                            as="button"
-                        >
-                            Log Out
-                        </ResponsiveNavLink>
-                    </div>
+                    <div></div>
                 </div>
             </div>
         </nav>
@@ -261,3 +167,4 @@
         </main>
     </div>
 </div>
+
